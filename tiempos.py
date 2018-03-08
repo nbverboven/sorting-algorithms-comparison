4c404e0d06ca544b7e15eca2a001973ddb1f938e
Nicolás Berríos Verboven <35927910+nbverboven@users.noreply.github.com>
2018-03-08 14:15:01 -0300

    Formateo de código y renombre de variables

--- tiempos.py
+++ tiempos.py
@@ -2,32 +2,27 @@ import sorting
 import random
 import time
 import copy
-import matplotlib.pyplot as plt
-import numpy as np
-
-def generar_puntos(n):
-	l = [random.random() for i in range(0, n)]
-	return l
 
 def generar_tupla(n):
-	l1 = generar_puntos(n)
-	l2 = copy.deepcopy(l1)
-	l3 = copy.deepcopy(l1)
-	l4 = copy.deepcopy(l1)
-	l5 = copy.deepcopy(l1)
-	x = time.clock()
-	sorting.insertionSort(l1)
-	y = time.clock()
-	sorting.selectionSort(l2)
-	z = time.clock()
-	sorting.bubbleSort(l3)
-	u = time.clock()
-	sorting.mergeSort(l4)
-	g = time.clock()
-	sorting.quickSort(l5)
-	h = time.clock()
-	return str(n), str(y-x), str(z-y), str(u-z), str(g-u), str(h-g)
+	lista_insertion = [random.random() for i in range(0, n)]
+	lista_selection = copy.deepcopy(lista_insertion)
+	lista_bubble = copy.deepcopy(lista_insertion)
+	lista_merge = copy.deepcopy(lista_insertion)
+	lista_quck = copy.deepcopy(lista_insertion)
+	tiempo_1 = time.clock()
+	sorting.insertionSort(lista_insertion)
+	tiempo_2 = time.clock()
+	sorting.selectionSort(lista_selection)
+	tiempo_3 = time.clock()
+	sorting.bubbleSort(lista_bubble)
+	tiempo_4 = time.clock()
+	sorting.mergeSort(lista_merge)
+	tiempo_5 = time.clock()
+	sorting.quickSort(lista_quck)
+	tiempo_6 = time.clock()
+	return str(n), str(tiempo_2-tiempo_1), str(tiempo_3-tiempo_2), str(tiempo_4-tiempo_3), str(tiempo_5-tiempo_4), str(tiempo_6-tiempo_5)
 
+		
 def generar_lista_tuplas(n, k):
 	ls = []
 	i = 0
