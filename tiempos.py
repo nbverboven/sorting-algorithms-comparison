feec306c20fcd4eae2a1232db8fdff9ae9b0af92
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-03-10 13:57:46 -0300

    Ahora, se devuelve una matriz con los tiempos para cada tamaño de arreglo en lugar de una lista de listas

--- tiempos.py
+++ tiempos.py
@@ -2,13 +2,15 @@ import sorting
 import random
 import time
 import copy
+import numpy as np
 
-def generar_tupla(n):
+def generar_tiempos(n):
 	lista_insertion = [random.random() for i in range(0, n)]
 	lista_selection = copy.deepcopy(lista_insertion)
 	lista_bubble = copy.deepcopy(lista_insertion)
 	lista_merge = copy.deepcopy(lista_insertion)
-	lista_quck = copy.deepcopy(lista_insertion)
+	lista_quick = copy.deepcopy(lista_insertion)
+
 	tiempo_1 = time.clock()
 	sorting.insertionSort(lista_insertion)
 	tiempo_2 = time.clock()
@@ -18,15 +20,21 @@ def generar_tupla(n):
 	tiempo_4 = time.clock()
 	sorting.mergeSort(lista_merge)
 	tiempo_5 = time.clock()
-	sorting.quickSort(lista_quck)
+	sorting.quickSort(lista_quick)
 	tiempo_6 = time.clock()
-	return str(n), str(tiempo_2-tiempo_1), str(tiempo_3-tiempo_2), str(tiempo_4-tiempo_3), str(tiempo_5-tiempo_4), str(tiempo_6-tiempo_5)
 
-		
-def generar_lista_tuplas(n, k):
-	ls = []
+	return np.asarray( (n, tiempo_2-tiempo_1, tiempo_3-tiempo_2, 
+		                   tiempo_4-tiempo_3, tiempo_5-tiempo_4, 
+		                   tiempo_6-tiempo_5) )
+
+
+def generar_matriz_tiempos(n, k):
+	ls = np.empty( (1, 6) )
 	i = 0
 	for i in range(10, n+1, k):
-		ls.append(generar_tupla(i))
+		if i == 10:
+			ls = generar_tiempos(i)
+		else:
+			ls = np.vstack( (ls, generar_tiempos(i)) )
 		i += 1
 	return ls
