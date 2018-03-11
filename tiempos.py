7cc14fad138fd6996029f9aad63e50df10461df3
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-03-11 18:47:55 -0300

    Cambié la función time.clock por time.process_time ya que la primera se encuentra deprecada desde la versión 3.3

--- tiempos.py
+++ tiempos.py
@@ -11,17 +11,17 @@ def generar_tiempos(n):
 	lista_merge = copy.deepcopy(lista_insertion)
 	lista_quick = copy.deepcopy(lista_insertion)
 
-	tiempo_1 = time.clock()
+	tiempo_1 = time.process_time()
 	sorting.insertionSort(lista_insertion)
-	tiempo_2 = time.clock()
+	tiempo_2 = time.process_time()
 	sorting.selectionSort(lista_selection)
-	tiempo_3 = time.clock()
+	tiempo_3 = time.process_time()
 	sorting.bubbleSort(lista_bubble)
-	tiempo_4 = time.clock()
+	tiempo_4 = time.process_time()
 	sorting.mergeSort(lista_merge)
-	tiempo_5 = time.clock()
+	tiempo_5 = time.process_time()
 	sorting.quickSort(lista_quick)
-	tiempo_6 = time.clock()
+	tiempo_6 = time.process_time()
 
 	return np.asarray( (n, tiempo_2-tiempo_1, tiempo_3-tiempo_2, 
 		                   tiempo_4-tiempo_3, tiempo_5-tiempo_4, 
@@ -33,8 +33,8 @@ def generar_matriz_tiempos(n, k):
 	i = 0
 	for i in range(10, n+1, k):
 		if i == 10:
-			ls = generar_tiempos(i)
+			ls = generar_tiempos(i) # Genero la primera fila de la matriz
 		else:
-			ls = np.vstack( (ls, generar_tiempos(i)) )
+			ls = np.vstack( (ls, generar_tiempos(i)) ) # Agrego la nueva fila abajo de la matriz
 		i += 1
 	return ls
